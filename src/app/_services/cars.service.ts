a0045bd248ea2dcdd1b75d6fe59348e5bdb8a3a7
alansastre <alan@alansastre.co>
2021-07-25 20:31:18 +0200

    configure backend url

--- src/app/_services/cars.service.ts
+++ src/app/_services/cars.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const API_URL = 'http://localhost:8080/api/';
+// const API_URL = 'http://localhost:8080/api/';
+const API_URL = 'https://alansastre-springboot-jwt.herokuapp.com/api/';
 
 @Injectable({
   providedIn: 'root'
