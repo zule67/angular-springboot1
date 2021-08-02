62082a3538b91a9feaffd9d5b6241072970a8276
carlos <carlos@DESKTOP-J61L43O.mshome.net>
2021-08-02 11:43:29 +0200

    commit

--- src/app/_services/hello.service.ts
+++ src/app/_services/hello.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 const API_URL = 'https://alansastre-springboot-jwt.herokuapp.com/api/';
+//const API_URL = 'https://m5-apirest-postgresql.herokuapp.com/api/';
 
 @Injectable({
   providedIn: 'root'
